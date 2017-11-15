2620dc775c295212f3f202d2ef7ec558d8ed592e
Ned Batchelder <ned@edx.org>
2017-11-15 14:14:10 -0500

    Check that SITE_NAME is acceptable from a edX trademark standpoint

---
+++ openedx/core/djangoapps/common_initialization/tests/test_domain.py
@@ -0,0 +1,15 @@
+from openedx.core.djangoapps.common_initialization.apps import acceptable_domain_name
+
+import pytest
+
+
+@pytest.mark.parametrize("domain, result", [
+    ("mysite.com", True),
+    ("mysite.com:8000", True),
+    ("edx.someguy.org", False),
+    ("openedx.someguy.org", True),
+    ("courses.edx.someguy.org", False),
+    ("courses.edx.someguy.org:18010", False),
+])
+def test_acceptable_domain_name(domain, result):
+    assert acceptable_domain_name(domain) == result
